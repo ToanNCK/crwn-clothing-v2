32ba34df2b561e92127261d4d319cd1cec4dc166
CYBEREYE\toan.nguyencongkhanh <toannck32@wru.vn>
2022-10-26 14:57:48 +0700

    update lesson 5

--- src/App.js
+++ src/App.js
@@ -1,35 +1,12 @@
-import Directory from "./components/directory/directory.component";
+import { Routes, Route } from 'react-router-dom';
+import Home from "./routes/home/home.component";
 
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: "Hats",
-      imageUrl: "https://i.ibb.co/cXC6V5v/0-Aura-1-1920x1080.png",
-    },
-    {
-      id: 2,
-      title: "Jackets",
-      imageUrl: "https://i.ibb.co/cXC6V5v/0-Aura-1-1920x1080.png",
-    },
-    {
-      id: 3,
-      title: "Sneakers",
-      imageUrl: "https://i.ibb.co/cXC6V5v/0-Aura-1-1920x1080.png",
-    },
-    {
-      id: 4,
-      title: "Womens",
-      imageUrl: "https://i.ibb.co/cXC6V5v/0-Aura-1-1920x1080.png",
-    },
-    {
-      id: 5,
-      title: "Mens",
-      imageUrl: "https://i.ibb.co/cXC6V5v/0-Aura-1-1920x1080.png",
-    },
-  ];
-
-  return <Directory categories={categories} />;
+  return (
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+    </Routes>
+  );
 };
 
 export default App;
---
+++ src/routes/home/home.component.jsx
@@ -0,0 +1,35 @@
+import Directory from "../../components/directory/directory.component";
+
+const Home = () => {
+  const categories = [
+    {
+      id: 1,
+      title: "Hats",
+      imageUrl: "https://i.ibb.co/cXC6V5v/0-Aura-1-1920x1080.png",
+    },
+    {
+      id: 2,
+      title: "Jackets",
+      imageUrl: "https://i.ibb.co/cXC6V5v/0-Aura-1-1920x1080.png",
+    },
+    {
+      id: 3,
+      title: "Sneakers",
+      imageUrl: "https://i.ibb.co/cXC6V5v/0-Aura-1-1920x1080.png",
+    },
+    {
+      id: 4,
+      title: "Womens",
+      imageUrl: "https://i.ibb.co/cXC6V5v/0-Aura-1-1920x1080.png",
+    },
+    {
+      id: 5,
+      title: "Mens",
+      imageUrl: "https://i.ibb.co/cXC6V5v/0-Aura-1-1920x1080.png",
+    },
+  ];
+
+  return <Directory categories={categories} />;
+};
+
+export default Home;
