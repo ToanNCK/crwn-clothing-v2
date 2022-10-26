9d5716687cec38a7f776fc70c1b4cff7dadcb786
CYBEREYE\toan.nguyencongkhanh <toannck32@wru.vn>
2022-10-26 10:59:17 +0700

    update lesson 3

--- src/App.js
+++ src/App.js
@@ -1,37 +1,40 @@
+import CategoryItem from "./components/category-item/category-item.component";
+
+import "./categories.styles.scss";
+
 const App = () => {
   const categories = [
     {
       id: 1,
-      title: 'Hats',
+      title: "Hats",
+      imageUrl: "https://i.ibb.co/cXC6V5v/0-Aura-1-1920x1080.png",
     },
     {
       id: 2,
-      title: 'Jackets',
+      title: "Jackets",
+      imageUrl: "https://i.ibb.co/cXC6V5v/0-Aura-1-1920x1080.png",
     },
     {
       id: 3,
-      title: 'Sneakers',
+      title: "Sneakers",
+      imageUrl: "https://i.ibb.co/cXC6V5v/0-Aura-1-1920x1080.png",
     },
     {
       id: 4,
-      title: 'Womens',
+      title: "Womens",
+      imageUrl: "https://i.ibb.co/cXC6V5v/0-Aura-1-1920x1080.png",
     },
     {
       id: 5,
-      title: 'Mens',
+      title: "Mens",
+      imageUrl: "https://i.ibb.co/cXC6V5v/0-Aura-1-1920x1080.png",
     },
   ];
 
   return (
-    <div className='categories-container'>
-      {categories.map(({ title }) => (
-        <div className='category-container'>
-          <div className='background-image' />
-          <div className='category-body-container'>
-            <h2>{title}</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
+    <div className="categories-container">
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
       ))}
     </div>
   );
