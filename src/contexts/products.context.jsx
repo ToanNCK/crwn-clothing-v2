bdb4d0cc9e3056971a2783661fd47ad95e4625a5
CYBEREYE\toan.nguyencongkhanh <toannck32@wru.vn>
2022-10-31 14:20:48 +0700

    update

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -11,7 +11,7 @@ export const ProductsProvider = ({ children }) => {
 
   useEffect(() => {
     const getCategoriesMap = async () => {
-      const categoryMap = await getCategoriesAndDocuments("collections");
+      const categoryMap = await getCategoriesAndDocuments("categories");
       console.log(categoryMap);
     };
 
